046bb89a4c0a2dac6fe5f9146c91622b5724deb3
jskim2376 <jskim2376@gmail.com>
2024-02-26 10:48:01 +0900

    Create Max Number of K-Sum Pairs.java

---
+++ Max Number of K-Sum Pairs.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == k) {
+                i++;
+                j--;
+                count++;
+            } else if (nums[i] + nums[j] < k) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return count;
+    }
+}
