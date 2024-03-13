025cb5b35314fee2de0849714e9b26bd0098a56b
jskim2376 <jskim2376@gmail.com>
2024-03-13 10:36:12 +0900

    Update 2352. Equal Row and Column Pairs.java

--- 2352. Equal Row and Column Pairs.java
+++ 2352. Equal Row and Column Pairs.java
@@ -3,18 +3,20 @@ class Solution {
         int result = 0;
         for(int row=0;row<grid.length; row++){
             for(int column=0;column<grid.length;column++){
-                boolean sameFlag = true;
-                for(int i=0;i<grid.length;i++){
-                    if(grid[row][i] != grid[i][column]){
-                        sameFlag=false;
-                        break;
-                    }
-                }
-                if(sameFlag)
+                if(isEqualRowAndColumn(grid,row,column))
                     result++;
             }
         }
 
         return result;
+    }
+
+    public boolean isEqualRowAndColumn(int[][] grid, int row, int column){
+        for(int i=0;i<grid.length;i++){
+            if(grid[row][i] != grid[i][column]){
+               return false;
+            }
+        }
+        return true;
     }
 }
