79f585a828637d39c2b003bd538f948c0e760811
jskim2376 <jskim2376@gmail.com>
2024-03-18 13:01:24 +0900

    Update 394. Decode String.java

--- 394. Decode String.java
+++ 394. Decode String.java
@@ -4,26 +4,34 @@ class Solution {
     public String decodeString(String s) {
         StringBuilder result = new StringBuilder();
         Stack<int[]> stack = new Stack<>();
-        int numberLeft = -1;
+        int repeatedLeft = -1;
 
         for (int i = 0; i < s.length(); i++) {
             char character = s.charAt(i);
 
+            // reapted가 여러 자리 일 수 있으니 숫자의 왼쪽 위치를 기억
             if (Character.isDigit(character)) {
-                if (numberLeft == -1) {
-                    numberLeft = i;
+                if (repeatedLeft == -1) {
+                    repeatedLeft = i;
                 }
+                // '[' 가 나오면 repeated를 구함
             } else if (character == '[') {
-                stack.push(new int[] { Integer.parseInt(s.substring(numberLeft, i)), result.length() });
-                numberLeft = -1;
+                int repeatedRight = i;
+                int repeated = Integer.parseInt(s.substring(repeatedLeft, repeatedRight));
+                // 반복할 문자열의 왼쪽 위치를 구하고, 반복할 값과 위치를 push
+                int repeatedStringLeft = result.length();
+                stack.push(new int[] { repeated, repeatedStringLeft });
+                repeatedLeft = -1;
             } else if (character == ']') {
-                int[] countAndLeft = stack.pop();
-                int count = countAndLeft[0];
-                int left = countAndLeft[1];
+                // encoded_string을 인코딩된 문자열을 repeated 만큼 반복
+                int[] repeatedAndLeft = stack.pop();
+                int repeated = repeatedAndLeft[0];
+                int left = repeatedAndLeft[1];
                 int right = result.length();
-                for (int j = 0; j < count - 1; j++) {
+                for (int j = 0; j < repeated - 1; j++) {
                     result.append(result.substring(left, right));
                 }
+                // 일반문자열이면 결과에 더함
             } else {
                 result.append(character);
             }
