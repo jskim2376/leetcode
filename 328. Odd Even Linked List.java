aeb461544a8d876e48781444daef4122e6f8c9dc
jskim2376 <jskim2376@gmail.com>
2024-03-22 14:32:20 +0900

    Create 328. Odd Even Linked List.java

---
+++ 328. Odd Even Linked List.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode odd = head, even = head.next, evenHead = head.next;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
