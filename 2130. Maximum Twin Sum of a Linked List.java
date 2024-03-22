9e5079f237a96e171a735d73319a78a78b12b0c8
jskim2376 <jskim2376@gmail.com>
2024-03-22 16:07:11 +0900

    Update 2130. Maximum Twin Sum of a Linked List.java

--- 2130. Maximum Twin Sum of a Linked List.java
+++ 2130. Maximum Twin Sum of a Linked List.java
@@ -13,3 +13,23 @@ class Solution {
         return max;
     }
 }
+
+// class Solution {
+// public int pairSum(ListNode head) {
+// int[] values = new int[100000];
+// int n = 0;
+// while (head != null) {
+// values[n] = head.val;
+// head = head.next;
+// n++;
+// }
+
+// int max = 0;
+// for (int i = 0; i <= (n / 2) - 1; i++) {
+// int sum = values[i] + values[(n - 1 - i)];
+// max = Math.max(max, sum);
+// }
+
+// return max;
+// }
+// }
