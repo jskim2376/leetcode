735960aefb0c5e7fa5b989c6ecb702c258254202
jskim2376 <jskim2376@gmail.com>
2024-03-25 12:07:03 +0900

    Update 872. Leaf-Similar Trees.java

--- 872. Leaf-Similar Trees.java
+++ 872. Leaf-Similar Trees.java
@@ -4,7 +4,6 @@ class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         Stack<Integer> root1Leaf = new Stack<>();
         getLeafNode(root1, root1Leaf);
-
         Stack<Integer> root2Leaf = new Stack<>();
         getLeafNode(root2, root2Leaf);
 
@@ -12,7 +11,8 @@ class Solution {
             return false;
 
         while (!root1Leaf.isEmpty()) {
-            if (root1Leaf.pop() != root2Leaf.pop()) {
+            // Integer를 == 비교하면 -128 ~ 127 까지의 값만 비교가능
+            if (!root1Leaf.pop().equals(root2Leaf.pop())) {
                 return false;
             }
         }
@@ -21,6 +21,7 @@ class Solution {
     }
 
     public void getLeafNode(TreeNode node, Stack<Integer> stack) {
+        // 마지막 노드 일경우 push
         if (node.left == null && node.right == null) {
             stack.push(node.val);
         }
