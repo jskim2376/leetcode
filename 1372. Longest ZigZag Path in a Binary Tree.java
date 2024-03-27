38bcea1f5037c70440eaaa1cd5f5a45dc5370de6
jskim2376 <jskim2376@gmail.com>
2024-03-27 16:26:40 +0900

    Update 1372. Longest ZigZag Path in a Binary Tree.java

--- 1372. Longest ZigZag Path in a Binary Tree.java
+++ 1372. Longest ZigZag Path in a Binary Tree.java
@@ -18,34 +18,31 @@
 // }
 
 class Solution {
+    // 가장 긴 지그재그 경로의 길이 저장
     int longestZigZagLength = 0;
 
     public int longestZigZag(TreeNode root) {
-        dfs(root, null, -1);
-        return longestZigZagLength;
+        dfs(root.left, Direction.LEFT, 0);
+        dfs(root.right, Direction.RIGHT, 0);
+        return longestZigZagLength; // 최장 경로 길이 반환
     }
 
-    enum Direction {
-        LEFT,
-        RIGHT
+    enum Direction { // 탐색 방향
+        LEFT, RIGHT
     }
 
     void dfs(TreeNode node, Direction direction, int currentLength) {
         if (node == null) {
-            longestZigZagLength = Math.max(longestZigZagLength, currentLength);
+            longestZigZagLength = Math.max(longestZigZagLength, currentLength); // 최대 길이 업데이트
             return;
         }
-
-        System.out.println(direction + " currentLength: " + currentLength);
+        // 왼쪽 또는 오른쪽으로 이동 시 방향 변경 및 경로 길이 계산
         if (direction == Direction.LEFT) {
-            dfs(node.right, Direction.RIGHT, currentLength + 1);
-            dfs(node.left, Direction.LEFT, 0);
-        } else if (direction == Direction.RIGHT) {
-            dfs(node.left, Direction.LEFT, currentLength + 1);
-            dfs(node.right, Direction.RIGHT, 0);
+            dfs(node.right, Direction.RIGHT, currentLength + 1); // 왼쪽에서 오른쪽으로 이동
+            dfs(node.left, Direction.LEFT, 0); // 경로 리셋
         } else {
-            dfs(node.left, Direction.LEFT, currentLength + 1);
-            dfs(node.right, Direction.RIGHT, currentLength + 1);
+            dfs(node.left, Direction.LEFT, currentLength + 1); // 오른쪽에서 왼쪽으로 이동
+            dfs(node.right, Direction.RIGHT, 0); // 경로 리셋
         }
     }
 }
