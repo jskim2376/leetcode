7ece3df21fa000aa8e6130a6800869ee3e342af4
jskim2376 <jskim2376@gmail.com>
2024-04-05 15:04:34 +0900

    Update 215. Kth Largest Element in an Array.java

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -1,19 +1,57 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+// import java.util.Collections;
+// import java.util.PriorityQueue;
+
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         var pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+//         for (int num : nums) {
+//             pq.add(num);
+//         }
+
+//         int result = 0;
+//         for (int i = 0; i < k; i++) {
+//             result = pq.poll();
+//         }
+
+//         return result;
+//     }
+// }
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        var pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        k = nums.length - k;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int pivotIndex = partion(nums, left, right);
 
-        for (int num : nums) {
-            pq.add(num);
+            // 탐색 범위를 줄인다
+            if (k == pivotIndex)
+                return nums[k];
+            else if (k < pivotIndex)
+                right = pivotIndex - 1;
+            else
+                left = pivotIndex + 1;
         }
+        return -1;
+    }
 
-        int result = 0;
-        for (int i = 0; i < k; i++) {
-            result = pq.poll();
+    int partion(int[] nums, int left, int right) {
+        swap(nums, left, new Random().nextInt(right - left + 1) + left);
+        int pivotIndex = left;
+        int pivot = nums[pivotIndex];
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < pivot)
+                swap(nums, i, ++pivotIndex);
         }
+        swap(nums, left, pivotIndex);
 
-        return result;
+        return pivotIndex;
+    }
+
+    void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
