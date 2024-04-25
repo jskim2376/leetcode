29fa0656d787c746dbe679d893d42da2dbee7180
jskim2376 <jskim2376@gmail.com>
2024-04-25 12:27:34 +0900

    Update 746. Min Cost Climbing Stairs.java
    Solution 2

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -1,13 +1,22 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        dp[dp.length - 1] = cost[cost.length - 1];
-        dp[dp.length - 2] = cost[cost.length - 2];
-
-        for (int i = cost.length - 3; i >= 0; i--) {
-            dp[i] = Math.min(dp[i + 1], dp[i + 2]);
+        // 배열 크기가 2일 경우, 두 값 중 작은 값을 반환
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
         }
 
-        return Math.min(dp[0], dp[1]);
+        // 마지막 두 요소로 초기화
+        int secondLast = cost[cost.length - 2];
+        int last = cost[cost.length - 1];
+
+        // 마지막 세 번째 요소부터 순회 시작
+        for (int i = cost.length - 3; i >= 0; i--) {
+            int current = cost[i] + Math.min(secondLast, last);
+            last = secondLast;
+            secondLast = current;
+        }
+
+        // 최소 시작 비용 계산
+        return Math.min(secondLast, last);
     }
 }
